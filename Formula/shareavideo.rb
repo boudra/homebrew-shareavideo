7b7a749d177067905174add03a3b7e43cb0862e9
Mohamed Boudra <boudra.moha@gmail.com>
2026-02-12 15:50:48 +0700

    Use proprietary license marker in formula

--- Formula/shareavideo.rb
+++ Formula/shareavideo.rb
@@ -3,7 +3,7 @@ class Shareavideo < Formula
   homepage "https://github.com/boudra/shareavideo"
   url "https://github.com/boudra/shareavideo/archive/refs/tags/v0.1.3.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  license "Unlicense"
+  license :cannot_represent
 
   depends_on :macos
 
